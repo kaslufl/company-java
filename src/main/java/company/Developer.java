50bdc9b4b6cc4380a3f6cb51b41698010022d58a
kaslufl <leallucasfonseca@gmail.com>
2021-09-17 20:24:07 -0300

    fix: Developer class and tests

--- src/main/java/company/Developer.java
+++ src/main/java/company/Developer.java
@@ -1,8 +1,25 @@
 package company;
 
-public class Developer extends Person{
+public class Developer extends Employee{
+    private Department department;
+
     public Developer(String name, Department department) {
-        super(name, department);
-        department.addDeveloper(this);
+        super(name);
+        setDepartment(department);
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        if (department == null) {
+            throw new NullPointerException("A department needs to be given.");
+        }
+        this.department = department;
+    }
+
+    public String getDepartmentDescription() {
+        return department.getDescription();
     }
 }
--- src/test/java/company/DeveloperTest.java
+++ src/test/java/company/DeveloperTest.java
@@ -2,14 +2,32 @@ package company;
 
 import org.junit.jupiter.api.Test;
 
+import static org.easymock.EasyMock.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeveloperTest {
     @Test
-    void shouldReturnDeveloper() {
-        Department department = new Department("Automation");
-        Developer developer = new Developer("Lucas", department);
+    void shouldReturnDepartmentDescription() {
+        Department department = createMock(Department.class);
+        expect(department.getDescription()).andReturn("Automation");
+        replay(department);
 
-        assertTrue(department.getDevelopers().contains(developer));
+        Developer developer = new Developer("Lucas",department);
+
+        assertEquals("Automation", developer.getDepartmentDescription());
+    }
+
+    @Test
+    void shouldThrowExceptionDepartmentNull() {
+        try{
+            Department department = createMock(Department.class);
+            Developer developer = new Developer("Lucas", department);
+            developer.setDepartment(null);
+            fail();
+        }
+        catch (NullPointerException e) {
+            assertEquals("A department needs to be given.", e.getMessage());
+        }
+
     }
 }
